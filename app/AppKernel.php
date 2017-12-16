f8ca2bf36a987ef799985949a214ca72ac05ca11
Sergey Protko <fesors@gmail.com>
2017-12-16 16:40:39 +0300

    Update PHP

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,7 +8,6 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
-            new Bernard\BernardBundle\BernardBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
